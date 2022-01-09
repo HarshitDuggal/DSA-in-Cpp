8ea985bde0bf6ca81366596cd445fe0321e06c39
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-01-09 21:47:42 +0000

    lect8 comp

---
+++ Basics of c++/functionex.cpp
@@ -0,0 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// int  power( int num1,int num2){
+
+//   int ans = 1;
+//   for(int i =1; i<=num2;i++){
+//     ans = ans*num1; 
+//   }
+//   return ans;
+// }
+// bool isEven(int a){
+  //if(a&1){// if odd 1 else 0 , 0 is false 1 is true
+//     return 0;
+//   }
+//   return 1;
+// }
+// int main(){
+//   int a,b;
+  // cin >> a >> b;
+  // int ans = power(a,b);
+  // cout << "power is"<< ans << endl;
+ 
+//     cin >> a;
+
+//     if(isEven(a)) {
+//         cout <<" Number is Even" <<endl;
+//     }
+//     else {
+//         cout<< " Number is Odd" <<endl;
+//     }
+  
+// }
---
+++ Basics of c++/ncr.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int factorial(int n){
+//   int fact=1;
+//   for(int i = 1;i<=n;i++){
+//     fact = fact * i;
+//   }
+//   return fact;
+// }
+// int nCr(int n, int r){
+//  int ans;
+//  ans = factorial(n)/(factorial(r)*factorial(n-r)); 
+//  return ans;
+// }
+// int main(){
+//   int a,b;
+//   cout << "Enter the value of n and r for combination";
+//   cin >> a >> b;
+//   cout << nCr(a,b);
+// }
