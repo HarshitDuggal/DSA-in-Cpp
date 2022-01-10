c0edf6cae94ce8abd1c508194447ba693bfec402
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-01-10 19:23:01 +0000

    hw lect 8 comp

--- Basics of c++/hw fibnth.cpp
+++ Basics of c++/hw fibnth.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
-int fibonaciNth(){
-
+int fibonaciNth(int n){
+int a=0,b=1,ans;
+for(int i=1;i<=n;i++){
+  ans = a+b;
+  a=b;
+  b=ans;
+}
+return b;
 }
 int main(){
-
+  int a;
+  cin >> a;
+  cout << "nth term of fibonaci series is " << fibonaciNth(a);
 }
