8a57da248585a65a9b6714946b02250f65536bd5
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-01-10 18:27:36 +0000

    hw

---
+++ Basics of c++/hw set bits.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+// int whileLoop(int a){
+//    int c,count = 0;
+//   while(a != 0){
+//   c = a % 2;
+//   if (c == 1){
+//     count++;
+//   }
+//   a=a/2;
+// }
+// return count;
+// }
+// int setBit(int a,int b){
+// return whileLoop(a)+ whileLoop(b);
+// }
+// int main(){
+//   int a,b;
+//   cout <<"Emter two no to know their set bits" ;
+//   cin >> a >> b;
+//   cout << setBit(a,b) ;
+// }
