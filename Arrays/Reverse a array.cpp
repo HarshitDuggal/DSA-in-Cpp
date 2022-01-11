492571bd9bfe13ae456dd4b64984991c61690af7
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-01-11 14:59:14 +0000

    hw2

--- Arrays/Reverse a array.cpp
+++ Arrays/Reverse a array.cpp
@@ -1,28 +1,28 @@
-#include<iostream>
-using namespace std;
-void printarray(int arr[] ,int size){
-  for(int i =0; i<size; i++){
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-}
-void reversearray(int arr[],int size){
-  int start=0,end=size-1;
-  while(start<=end){
-    swap(arr[start],arr[end]);
-    start++;
-    end--;
-  }
-}
-int main(){
-  int arr[5] = {1,2,3,4,5},brr[6]={7,4,0,9,3,1};
-  cout << endl << "normal array" << endl;
-  printarray(arr,5);
-  printarray(brr,6);
+// #include<iostream>
+// using namespace std;
+// void printarray(int arr[] ,int size){
+//   for(int i =0; i<size; i++){
+//     cout << arr[i] << " ";
+//   }
+//   cout << endl;
+// }
+// void reversearray(int arr[],int size){
+//   int start=0,end=size-1;
+//   while(start<=end){
+//     swap(arr[start],arr[end]);
+//     start++;
+//     end--;
+//   }
+// }
+// int main(){
+//   int arr[5] = {1,2,3,4,5},brr[6]={7,4,0,9,3,1};
+//   cout << endl << "normal array" << endl;
+//   printarray(arr,5);
+//   printarray(brr,6);
 
-  reversearray(arr,5);
-  reversearray(brr,6);
-  cout << endl << "reverse array" << endl;
-  printarray(arr,5);
-  printarray(brr,6);
-}
+//   reversearray(arr,5);
+//   reversearray(brr,6);
+//   cout << endl << "reverse array" << endl;
+//   printarray(arr,5);
+//   printarray(brr,6);
+// }
---
+++ Arrays/sumofallinarr.cpp
@@ -0,0 +1,10 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int arr[10] ={1,123,-12,234,-123,23,12,3,5,10};
+//   int ans = 0;
+//   for(int i=0; i<10;i++){
+//       ans = ans + arr[i];        
+//     } 
+//     cout << ans;
+// }
