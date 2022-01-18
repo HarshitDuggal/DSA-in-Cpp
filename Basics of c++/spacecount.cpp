2ee05ebf9f8bdc6096183f9062bbe5831ad67e72
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-01-18 20:09:54 +0000

    college

---
+++ Basics of c++/spacecount.cpp
@@ -0,0 +1,16 @@
+// // Write a program in C/C++ to count number of spaces in a line.
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main(){
+//   int space=0;
+//   string line;
+//     cout<<"Enter a string\n";
+//     getline(cin,line);
+//     for(int i=0; i<line.length(); i++){
+//  if(line[i]==' '){
+//         space++;
+//     }
+// }
+// cout<<"Total no of space is "<<space;
+// }
