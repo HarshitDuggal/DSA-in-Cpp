cdc7458df4c1a3dbf41f825c025508c53fcb3f18
Harshit Duggal <75092196+HarshitDuggal@users.noreply.github.com>
2022-06-16 19:10:13 +0000

    2d array

---
+++ Arrays/2darray.cpp
@@ -0,0 +1,62 @@
+// #include <iostream>
+
+// using namespace std;
+
+// bool isPresent(int arr[][4],int target,int m,int n){
+//   for(int i=0;i<3;i++){
+//     for(int j=0;j<4;j++){
+//       if(target == arr[i][j]){
+//         return 1;
+//       }
+//     }
+//   }
+//   return 0;
+// }
+
+// // int printSum(int arr[][4],int m,int n){
+// //   for(int i=0;i<3;i++){
+// //     for(int j=0;j<4;j++){
+// //       if(target == arr[i][j]){
+// //         return 1;
+// //       }
+// //     }
+// //   }
+// //   return 0;
+// // }
+
+// int main(){
+//   int arr[3][4];
+
+//   // taking output
+//   for(int i=0;i<3;i++){
+//     for(int j=0;j<4;j++){
+//       cin >> arr[i][j];
+//     }
+//   }
+
+//   for(int i=0;i<4;i++){
+//     for(int j=0;j<3;j++){
+//       cin >> arr[j][i];
+//     }
+//   }
+  
+//   //taking output
+//   for(int i=0;i<3;i++){
+//     for(int j=0;j<4;j++){
+//       cout << arr[i][j];
+//     }
+//    cout << endl;
+//   }
+
+//   cout << "Enter a no to find" << endl ;
+//   int target;
+//   cin >> target;
+
+//   if(isPresent(arr,target,3,4)){
+//     cout << "Element is present" << endl;
+//   }
+//   else{
+//     cout << "No Element is present" ;
+//   }
+  
+// }
---
+++ Arrays/chararray.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//    // string first;
+//   //  char second[10];
+  
+//   // second[0]='a';
+//   // second[1]='b';
+//   // second[2]='\0';
+//   // second[3]='c';
+//   // second[4]='d';
+//   // second[5]='\0';
+
+  
+
+//   // // cout << first <<endl;
+//   // cout << second <<endl;
+//   int a=0, b=5;
+  
+//   a=~b;
+//   cout << a;
+// }
